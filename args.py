ae2a202a3607e1dd7e2810d6bf7f23d4d2c960a3
Matheus Abreu <matheusa596@gmail.com>
2022-11-28 13:24:35 -0300

    Adicionando outros arquivos do struct2vec.

--- args.py
+++ args.py
@@ -1,13 +1,19 @@
 class Args:
+    qtd = 0
     def __init__(self, input) -> None:
         self.input = [(e[0], e[1]) for e in input]
         self.weighted = False
         self.directed = False
         self.p = 1
         self.q = 1
-        self.num_walks = 10
-        self.walk_length = 30
-        self.dimensions = 20
-        self.window_size = 10
+        self.num_walks = 5
+        self.walk_length = 10
+        self.dimensions = 10
+        self.window_size = 5
         self.workers = 8
         self.iter = 1
+        self.OPT1 = True
+        self.OPT2 = True
+        self.OPT3 = False
+        Args.qtd += 1
+        print(Args.qtd)
